c6704fc0f9acc23da67dff533801b2e5685bdd7c
Gonsowo <81293456+Gonsowo@users.noreply.github.com>
2023-11-19 00:42:33 +0100

    Cosas

--- Tale Weavers - Prototype/Assets/Scripts/MainMenuScript.cs
+++ Tale Weavers - Prototype/Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,17 @@
+using DG.Tweening;
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenuScript : MonoBehaviour
 {
-   private void Start()
+    [SerializeField] private RectTransform optionsMenu;
+    [SerializeField] private CanvasGroup optionsMenuImage;
+
+    private void Start()
     {
         if (!AudioManager.instance.IsPlaying("musicaMenu")) AudioManager.instance.Play("musicaMenu");
         AudioManager.instance.Stop("musicaVictoria");
@@ -21,5 +27,17 @@ public class MainMenuScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(optionsMenuImage.DOFade(1, 0.5f).SetEase(Ease.InCubic));
+    }
+
+    public void CloseOptions()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(optionsMenuImage.DOFade(0, 0.5f).SetEase(Ease.InCubic));
     }
 }
